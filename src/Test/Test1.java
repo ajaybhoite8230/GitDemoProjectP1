673d5c5a539c360bbc27fbc09da4f82109f7ac00
44777 <Ajbhoite2190>
2022-10-28 15:04:58 +0530

    First commit...

---
+++ src/Test/Test1.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry="Yes";
+		
+		if (hungry=="Yes")
+			System.out.println("Eat someting...");
+		else 
+		{
+			System.out.println("Do work....");
+		}
+
+	}
+
+}
